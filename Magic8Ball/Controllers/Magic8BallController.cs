20b55b65a869eb2f8ece81369ec4aa6803acec18
Jared Williams <Williams.jaredk@gmail.com>
2025-07-09 20:42:44 -0600

    Initial Changes

---
+++ Magic8Ball/Controllers/Magic8BallController.cs
@@ -0,0 +1,34 @@
+﻿using Magic8Ball.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Magic8Ball.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class Magic8BallController : ControllerBase
+	{
+		[Route("v1/ask")]
+		[HttpGet]
+		public ActionResult<Wisdom> Get(string question)
+		{
+			if(question == null)
+			{
+				return NotFound();
+			}
+
+			if(question == string.Empty) 
+			{ 
+				return BadRequest(); 
+			}
+
+			Wisdom response = new Wisdom
+			{
+				Question = question,
+				Id = Guid.NewGuid(),
+				Answer = Wisdom.GetAnswer()
+			};
+			return Ok(response);
+		}
+	}
+}
---
+++ Magic8Ball/Models/Wisdom.cs
@@ -0,0 +1,14 @@
+﻿namespace Magic8Ball.Models
+{
+	public class Wisdom
+	{
+		public Guid Id { get; set; }
+		public required string Question { get; set; }
+		public string Answer { get; set; } = string.Empty;
+
+		public static string GetAnswer()
+		{
+			return Answers.GetRandomAnswer();
+		}
+	}
+}
